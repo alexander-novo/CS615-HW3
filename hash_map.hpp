eefd4058356d7e98eeee59d931973faee8cba443
Alexander Novotny <alexander_novo@mail.tmcc.edu>
2020-04-08 09:42:49 -0700

    Added some comments

--- hash_map.hpp
+++ hash_map.hpp
@@ -5,13 +5,21 @@
 #include "kmer_t.hpp"
 
 struct HashMap {
+	// Vector of pointers to data. One pointer for each process,
+	// but each pointer points to an array of sizePerProcess elements,
+	// so elements are distributed accross processes
 	std::vector<upcxx::global_ptr<kmer_pair>> data;
+	// Vector of 'used' flags which keep track of which slots in data
+	// contain valid data. Global pointers for the purpose of atomic
+	// operations. 0 if the element is unused, 1 otherwise.
 	std::vector<upcxx::global_ptr<unsigned>> used;
+	// The number of elements pointed to by each data/used pointer
+	size_t sizePerProcess;
+	// An atomic domain for accessing the used flags atomically
 	upcxx::atomic_domain<unsigned> ad;
-	unsigned sizePerProcess;
 
+	// total # of elements in the hashmap
 	size_t my_size;
-
 	size_t size() const noexcept;
 
 	HashMap(size_t size);
@@ -59,6 +67,7 @@ bool HashMap::insert(const kmer_pair &kmer) {
 	uint64_t hash  = kmer.hash();
 	uint64_t probe = 0;
 	bool success   = false;
+
 	do {
 		uint64_t slot = (hash + probe++) % size();
 		success       = request_slot(slot);
@@ -70,17 +79,16 @@ bool HashMap::insert(const kmer_pair &kmer) {
 bool HashMap::find(const pkmer_t &key_kmer, kmer_pair &val_kmer) {
 	uint64_t hash  = key_kmer.hash();
 	uint64_t probe = 0;
-	bool success   = false;
+	bool success   = false, fullSlot;
+
 	do {
 		uint64_t slot = (hash + probe++) % size();
-		if (slot_used(slot)) {
+		fullSlot      = slot_used(slot);
+		if (fullSlot) {
 			val_kmer = read_slot(slot);
 			if (val_kmer.kmer == key_kmer) { success = true; }
-			// Performance-saving measure
-		} else {
-			break;
 		}
-	} while (!success && probe < size());
+	} while (!success && probe < size() && !fullSlot);
 	return success;
 }
 
@@ -97,6 +105,8 @@ kmer_pair HashMap::read_slot(uint64_t slot) {
 }
 
 bool HashMap::request_slot(uint64_t slot) {
+	// Expect the value of 0 and set to 1. If it returns 0, it means that the slot was unused
+	// and we have succesfully requested it.
 	return ad.compare_exchange(used[slot / sizePerProcess] + (slot % sizePerProcess), 0, 1,
 	                           std::memory_order_relaxed)
 	           .wait() == 0;
